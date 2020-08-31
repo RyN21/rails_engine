f75f2efa9bbd58954210118fc524e8a216946454
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-31 15:13:06 -0600

    Create test for items create and update endpoints

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Items API" do
-  it "sends a list of items" do
+  it "index of items" do
     merch = create(:merchant)
     create_list(:item, 3, merchant: merch)
 
@@ -11,7 +11,7 @@ describe "Items API" do
     expect(items.size).to eq(3)
   end
 
-  it "can get an items show page" do
+  it "show page of item" do
     merch = create(:merchant)
     id = create(:item, merchant: merch).id
 
@@ -19,5 +19,35 @@ describe "Items API" do
     item = JSON.parse(response.body)
     expect(response).to be_successful
     expect(item["id"]).to eq(id)
+  end
+
+  it "create an item" do
+    merch = create(:merchant)
+    item_params = {name: "Quick Draw",
+                   description: "Petzl, high quality draws",
+                   merchant: merch.id}
+
+    post '/api/v1/items', params: {item_params}
+    item = Item.last
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+  end
+
+  it "update a record of an item" do
+    merch = create(:merchant)
+    id = create(:item, merchant: merch)
+    previous_name = Item.last.name
+    item_params = { name: "Quick Draw" }
+
+    put "/api/v1/items/#{id}", params: {item_params}
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Quick Draws")
+  end
+
+  it "destroy an item" do
+    
   end
 end
