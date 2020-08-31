dbc1136214469f77dd931b7bf222cc83dfa969fa
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-31 15:21:51 -0600

    Create endpoint for items create

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -25,32 +25,34 @@ describe "Items API" do
     merch = create(:merchant)
     item_params = {name: "Quick Draw",
                    description: "Petzl, high quality draws",
-                   merchant: merch.id}
+                   unit_price: 7795,
+                   merchant_id: merch.id}
 
-    post '/api/v1/items', params: {item_params}
+    post '/api/v1/items', params: {item: item_params}
     item = Item.last
     expect(response).to be_successful
     expect(item.name).to eq(item_params[:name])
   end
 
-  it "update a record of an item" do
-    merch = create(:merchant)
-    id = create(:item, merchant: merch)
-    previous_name = Item.last.name
-    item_params = { name: "Quick Draw" }
-
-    put "/api/v1/items/#{id}", params: {item_params}
-    item = Item.find_by(id: id)
-
-    expect(response).to be_successful
-    expect(item.name).to_not eq(previous_name)
-    expect(item.name).to eq("Quick Draws")
-  end
-
-  it "destroy an item" do
-    merch = create(:merchant)
-    item = create(:item, merchant: merch)
-
-    expect{ delete "/api/v1/items/#{@item.id}" }.to change(Item, :count).by(-1)
-  end
+  # it "update a record of an item" do
+  #   merch = create(:merchant)
+  #   id = create(:item, merchant: merch)
+  #   previous_name = Item.last.name
+  #   item_params = { name: "Quick Draw" }
+  #
+  #   put "/api/v1/items/#{id}", params: {item_params}
+  #   item = Item.find_by(id: id)
+  #
+  #   expect(response).to be_successful
+  #   expect(item.name).to_not eq(previous_name)
+  #   expect(item.name).to eq("Quick Draws")
+  # end
+  #
+  # it "destroy an item" do
+  #   merch = create(:merchant)
+  #   item = create(:item, merchant: merch)
+  #
+  #   expect{ delete "/api/v1/items/#{@item.id}" }.to change(Item, :count).by(-1)
+  #   expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  # end
 end
