ededf57e7204d67ee3bf74f2473055722ecea48a
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-31 14:53:35 -0600

    Expose end points for items index and items show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 describe "Items API" do
   it "sends a list of items" do
-    create_list(:item, 3)
+    merch = create(:merchant)
+    create_list(:item, 3, merchant: merch)
 
     get '/api/v1/items'
-
     expect(response).to be_successful
+    items = JSON.parse(response.body)
+    expect(items.size).to eq(3)
+  end
+
+  it "can get an items show page" do
+    merch = create(:merchant)
+    id = create(:item, merchant: merch).id
+
+    get "/api/v1/items/#{id}"
+    item = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(item["id"]).to eq(id)
   end
 end
